6b7831a1a2ebf9a10610a5ba71f45cbdb15a513c
pxl_head <132537166+pxl-head@users.noreply.github.com>
2026-09-18 12:48:02 +0300

    Update pxl_head portfolio credit link

--- src/sections/SiteCredit.tsx
+++ src/sections/SiteCredit.tsx
@@ -1,4 +1,4 @@
-const PXL_HEAD_SITE_URL = "https://t.me/pxl_head";
+const PXL_HEAD_SITE_URL = "https://pxl-head.github.io/pxl-head-portfolio/?v=6baac57#photo";
 
 interface SiteCreditProps {
   className?: string;
